60c4ec1aa57933cda0d5b9086238362cc80ab7b1
Ananthiselvi <37336021+Ananthiselvi@users.noreply.github.com>
2018-04-30 12:19:18 +0530

    Create distinct and count.sql

---
+++ distinct and count.sql
@@ -0,0 +1,11 @@
+create database Ananthi;
+show databases;
+create table student(name char(25),roll_no varchar(8),sex char(1),dept char(5));
+insert into student values("ananthi","15bee084","f","eee");
+insert into student values("ananthi","15bee084","f","eee");
+insert into student values("sudha","15bee085","f","eee");
+select * from student;
+select count(*) from student;
+desc student;
+select distinct * from student;
+desc student;
