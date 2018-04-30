8706df3f05ffe65be0b65b09e8a8fb4101c47c03
Ananthiselvi <37336021+Ananthiselvi@users.noreply.github.com>
2018-04-30 11:40:20 +0530

    Create primary key.sql

---
+++ primary key.sql
@@ -0,0 +1,10 @@
+create database company;
+show databases;
+create table employee(
+    name char(25),
+    id_num int(10),
+    age int(3),
+    dept char(10),
+    primary key(id_num)
+);
+desc employee;
