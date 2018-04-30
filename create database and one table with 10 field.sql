bd8c1f05a897d0dec1855451bdd65e15974f6485
Ananthiselvi <37336021+Ananthiselvi@users.noreply.github.com>
2018-04-30 09:17:39 +0530

    Create create database and one table with 10 field.sql

---
+++ create database and one table with 10 field.sql
@@ -0,0 +1,4 @@
+create database Ananthiselvi;
+show databases;
+create table details(name char(25),roll_no varchar(8),dept char(5),mobile_number int(10) not null,address varchar(50),father_name char(30),mail_id varchar(25),blood_group varchar(8),date_of_birth float(15),age int(2));
+desc details;
