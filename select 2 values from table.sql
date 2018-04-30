da3f76b4f2020af46d13739205ac74afb13ad6ef
Ananthiselvi <37336021+Ananthiselvi@users.noreply.github.com>
2018-04-30 09:18:27 +0530

    Create select 2 values from table.sql

---
+++ select 2 values from table.sql
@@ -0,0 +1,6 @@
+create database Ananthiselvi;
+show databases;
+create table details(name char(25),roll_no varchar(8),dept char(5),college_name char(40));
+desc details;
+insert into details values("Ananthiselvi","15bee084","eee","KCT");
+select name,roll_no from details;
